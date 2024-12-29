425ec1c951e37ad42319854353e274b81344d951
harshy261 <iyadavharsh261@gmail.com>
2024-12-29 13:38:14 +0530

    JVM / JDK / JRE

---
+++ 01_Basics/18_JDK_JRE_JVM/Jdk.java
@@ -0,0 +1,11 @@
+
+public class Jdk {
+    public static void main(String[] args) {
+        System.out.println("JDK: Java Development Kit");
+        System.out.println("JRE: Java Runtime Environment");
+        System.out.println("JVM: Java Virtual Machine");
+        System.out.println("JDK = JRE + Development Tools");
+        System.out.println("JRE = JVM + Library Classes");
+        System.out.println("JVM = Interpreter");
+    }
+}

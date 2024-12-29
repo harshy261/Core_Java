4077a64993e536537560331420d39b0bcddb0eb7
harshy261 <iyadavharsh261@gmail.com>
2024-12-30 03:46:30 +0530

    Wrapper Class

--- 02_OOPs/14_Casting/Casting.java
+++ 02_OOPs/14_Casting/Casting.java
@@ -1,5 +1,34 @@
-package 02_OOPs.14_Casting;
+class A
+{
+	public void show1()
+	{
+		System.out.println("in A show");
+	}
+}
+class B extends A
+{
+	public void show2()
+	{
+		System.out.println("in show B");
+	}
+}
 
 public class Casting {
-    
+    public static void main(String[] args) {
+
+        //    	double d=4.5;
+        //    	int i=(int)d;
+        //    	
+        //    	System.out.println(i);
+                
+        //    	A obj= new A();
+        //    	A obj=(A) new B();   //upcasting
+        //    	obj.show1();
+                
+                A obj=new B();
+                obj.show1();
+                
+                B obj1=(B)obj;
+                obj1.show2();
+            }
 }
